2b2ab2b69e4d02809815679de60263dabc5f2d68
ml-brent <brent.mc@medialand.tw>
2021-03-31 18:05:07 +0800

    watchContentBase 設定為 true, html 內容改變時會自動重新整理

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,6 +1,6 @@
 import '../sass/main.scss'
 import '../js/main'
 
-if (process.env.NODE_ENV !== 'production') {require('../pages/contact.pug')}
+// if (process.env.NODE_ENV !== 'production') {require('../pages/contact.pug')}
 
 console.log('contact.js');
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,6 @@ import '../js/main'
 // import './img/index/picture.jpg'
 import '../img/index/colin-lloyd.jpeg'
 
-if (process.env.NODE_ENV !== 'production') {require('../pages/index.pug')}
+// if (process.env.NODE_ENV !== 'production') {require('../pages/index.pug')}
 
 console.log('index.js');
